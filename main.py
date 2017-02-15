b6d91d3418faa0a65912c6c213a17d4a98265c4e
L Gonzalez <luisalbertogonz91@gmail.com>
2017-02-15 14:26:27 -0600

    Hitting sign-up directs you to signup page. Added bars for Username, password, verify, and email.

--- main.py
+++ main.py
@@ -17,20 +17,46 @@
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
+
     def get(self):
-        head = "<head><style>.error{color:red;}</style></head>"
-        Username= "<tr><td><label for='username'>Username</label><td><td><input name='username' type ='text' value required><span class='error'></span></td></tr>"
-        Password= "<tr><td><label for='password'>Password</label></td><td><input name='password' type ='text' value required><span class='error'></span></td></tr>"
-        Verify_password= "<tr><td><label for 'verify password'>Verify Password</label></td><td><input name='Verify password' type='text' required><span class='error'></span></td></tr>"
-        Email= "<tr><td><label for='email'>E-mail(Optional)</label></td><td><input name='email' type='text' value required><span class='error'></span></td></tr>"
-        formarea = "<form method='post'></table></tbody>"+Username+"<br>"+Password+"<br>"+Verify_password+"<br>"+Email+"</tbody><br><input type='submit'></form>"
-        body = "<body><h1>Signup</h1></body>" + formarea
-        content = head + body
-        self.response.write(content)
+        #head = "<head><style>.error{color:red;}</style></head>"
+        #Username= "<tr><td><label for='username'>Username</label><td><td><input name='username' type ='text' value required><span class='error'></span></td></tr>"
+        #Password= "<tr><td><label for='password'>Password</label></td><td><input name='password' type ='text' value required><span class='error'></span></td></tr>"
+        #Verify_password= "<tr><td><label for 'verify password'>Verify Password</label></td><td><input name='Verify password' type='text' required><span class='error'></span></td></tr>"
+        #Email= "<tr><td><label for='email'>E-mail(Optional)</label></td><td><input name='email' type='text' value required><span class='error'></span></td></tr>"
+        #formarea = "<form method='post'></table></tbody>"+Username+"<br>"+Password+"<br>"+Verify_password+"<br>"+Email+"</tbody><br><input type='submit'></form>"
+        #body = "<body><h1>Signup</h1></body>" + formarea
+        #content = head + body
+        header = """<h2>I want to sign-up</h2>"""
+        form = """<form action='/signup' method='post'>
+                <label>Username:<input type='text' name='username'/></label>
+                <br>
+                <label>Password:<input type='text' name='password'/><label>
+                <br>
+                <label>Verify Password:<input type='text' name='verify_password'/></label>
+                <br>
+                <label>E-mail(Optional):<input type='text' name='e-mail'/><label>
+                <br>
+                <input type='submit' value="signup"/></form>"""
+
+        self.response.write(header+form)
+
+class SignupHandler(webapp2.RequestHandler):
+
+    def post(self):
+        header = '<h2>Sign-up</h2>'
+        username = self.request.get('username')
+        signup_confirm = username +' has signed up.'
+
+        self.response.write(header+signup_confirm)
 
 #class LoginHandler(webapp2.LoginHandler):
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    #('/login',LoginHandler)
+    ('/signup',SignupHandler),
 ], debug=True)
+#app = webapp2.WSGIApplication(  [
+#    ('/', MainHandler),
+#    #('/login',LoginHandler)
+#], debug=True)
